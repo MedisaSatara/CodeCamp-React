8328509436ed5c7704b3111708a5e800310607a3
MedisaSatara <92315913+MedisaSatara@users.noreply.github.com>
2024-10-16 20:10:59 +0200

    update code

--- my-react-app/src/InputForm.tsx
+++ my-react-app/src/InputForm.tsx
@@ -16,6 +16,10 @@ export const InputForm: React.FC = () => {
       />
       <h2>Pozdrav, {name}.</h2>
       <br />
+      <div>
+        <h1>{isTemaTamna ? "Dark Mode" : "Light Mode"}</h1>
+        <button onClick={() => setIsTemaTamna(!isTemaTamna)}>Toggle</button>
+      </div>
     </div>
   );
 };
